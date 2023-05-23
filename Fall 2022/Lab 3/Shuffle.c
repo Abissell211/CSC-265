3b452bb2204db52ff3f41a7bb023cfef635ab7ea
Abissell211 <123478376+Abissell211@users.noreply.github.com>
2023-05-23 14:31:46 -0400

    Add files via upload

---
+++ Fall 2022/Lab 3/Shuffle.c
@@ -0,0 +1,15 @@
+/*
+ *  Encryption Program
+ *
+ *	Digital Clock Program:
+ *  	22-Sep-22 : 
+ *		Andrew Bissell
+ *		Joshua Hughes
+ *		Noah Proctor
+ *	
+ *	Acknowledgements: https://stackoverflow.com/questions/1247989/how-do-you-allow-spaces-to-be-entered-using-scanf
+ */
+ 
+#include <stdio.h>
+
+void shuffle(char *array, size_t n);
